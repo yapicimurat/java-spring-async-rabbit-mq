c614e47ed595a0053a3c00202f8c78ebc711c243
Murat YAPICI <muratyapiicii@gmail.com>
2025-11-26 22:47:38 +0300

    Setup spring project

---
+++ src/main/java/org/example/javaspringrabbitmqmonitoring/JavaSpringRabbitmqMonitoringApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaspringrabbitmqmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRabbitmqMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringRabbitmqMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaspringrabbitmqmonitoring/JavaSpringRabbitmqMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaspringrabbitmqmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRabbitmqMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
